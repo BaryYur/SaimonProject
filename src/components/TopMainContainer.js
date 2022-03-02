dba6851d311dc130d4c8bded629960ce30569d0f
BaryYur <73163625+BaryYur@users.noreply.github.com>
2022-03-02 19:44:22 +0200

    commit

--- src/components/TopMainContainer.js
+++ src/components/TopMainContainer.js
@@ -11,11 +11,13 @@ function MainContainer () {
     return (
         <div className='top-main-container'>
             <div className='top-main'>
-                <div>
-                    <h2>The way you feel comportable</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor morbi vulputate nec amet purus</p>
+                <div className='lf__top-main'>
+                    <div>
+                        <h2>The way you feel comportable</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor morbi vulputate nec amet purus</p>
+                    </div>
                 </div>
-                <div>
+                <div className='c_top-main'>
                     <img src={search} alt='search-icon' />
                     <h3>Simply do it by yourself</h3>
                     <p>With a large database of influencers at your disposal, find those who match your own criteria, set up and manage campaigns and get the results you want for your business, no matter the size.</p>
@@ -23,7 +25,7 @@ function MainContainer () {
                         <img src={whiteArrow} alt='white-arrow' />
                     </Link>
                 </div>
-                <div>
+                <div className='rg_top-main'>
                     <img src={setting} alt='setting-icon' />
                     <h3>Let us handle it for you</h3>
                     <p>You set the objectives, and we handle your campaigns from A to Z. Let the team of industry experts bring your objectives to life through high-quality campaigns with guaranteed ROI.                         </p>
