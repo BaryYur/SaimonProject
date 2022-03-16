bd19ca62ea3e0b81b8cdcd37c290c48f30df068a
BaryYur <73163625+BaryYur@users.noreply.github.com>
2022-03-16 12:02:44 +0200

    commit

--- src/components/TopMainContainer.js
+++ src/components/TopMainContainer.js
@@ -21,22 +21,34 @@ function MainContainer () {
                     </div>
                 </div>
                 <div className='right__top-main-container'>
-                    <div className='c_top-main' data-aos='flip-up' data-aos-delay='600'>
+                    <Link   
+                        to='/' 
+                        className='c_top-main' 
+                        data-aos='flip-up' 
+                        data-aos-delay='610'
+                    >
                         <img src={search} alt='search-icon' />
                         <h3>Simply do it by yourself</h3>
                         <p>With a large database of influencers at your disposal, find those who match your own criteria, set up and manage campaigns and get the results you want for your business, no matter the size.</p>
-                        <Link to='/'>
+                        <div className='arrow-box'>
+                            <span></span>
                             <img src={whiteArrow} alt='white-arrow' />
-                        </Link>
-                    </div>
-                    <div className='rg_top-main' data-aos='flip-up' data-aos-delay='600'>
+                        </div>
+                    </Link>
+                    <Link 
+                        to='/' 
+                        className='rg_top-main' 
+                        data-aos='flip-up' 
+                        data-aos-delay='600'
+                    >
                         <img src={setting} alt='setting-icon' />
                         <h3>Let us handle it for you</h3>
                         <p>You set the objectives, and we handle your campaigns from A to Z. Let the team of industry experts bring your objectives to life through high-quality campaigns with guaranteed ROI.                         </p>
-                        <Link to='/'>
+                        <div className='arrow-box'>
+                            <span></span>
                             <img src={blackArrow} alt='black-arrow' />
-                        </Link>
-                    </div>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
@@ -47,22 +59,20 @@ export default MainContainer;
 
 AOS.init({
   // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  disable: false, 
+  startEvent: 'DOMContentLoaded', 
+  initClassName: 'aos-init', 
+  animatedClassName: 'aos-animate',
+  useClassNames: false, 
+  disableMutationObserver: false, 
+  debounceDelay: 50, 
+  throttleDelay: 99, 
   
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 50, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 800, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: true, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  offset: 50,
+  delay: 0, 
+  duration: 800, 
+  easing: 'ease', 
+  once: true, 
+  mirror: false,
+  anchorPlacement: 'top-bottom'
 });
